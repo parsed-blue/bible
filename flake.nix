4bde8bb647fb430a0d96058db87bce18f74f652b
ZehCnaS34 <alex@violet.city>
2025-11-18 23:04:45 -0500

    eachSystem

--- flake.nix
+++ flake.nix
@@ -4,18 +4,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     naersk.url = "github:nix-community/naersk";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, naersk }: let
+  outputs = { self, nixpkgs, naersk, systems }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
     pkgs = nixpkgs.legacyPackages."aarch64-darwin";
     naerskLib = pkgs.callPackage naersk {};
   in {
+    packages = eachSystem (system: {
+      default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = [ pkgs.glib ];
+        nativeBuildInputs = [ pkgs.pkg-config ];
+      };
+    });
     # packages.aarch64-darwin.default = pkgs.callPackage ./default.nix { };
-    packages.aarch64-darwin.default = naerskLib.buildPackage {
-      src = ./.;
-      buildInputs = [ pkgs.glib ];
-      nativeBuildInputs = [ pkgs.pkg-config ];
-    };
     devShells.aarch64-darwin.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         cargo rustc rustfmt clippy rust-analyzer glib
