d765b17230f108ef64958df2782c6acbd6b89fa6
ZehCnaS34 <alex@violet.city>
2025-11-21 13:09:50 -0500

    add build revision

--- build.rs
+++ build.rs
@@ -1,17 +1,45 @@
 use std::process::Command;
+use std::env;
+
+struct Git;
+
+impl Git {
+    fn rev_parse() -> Result<String, &'static str> {
+        let git_hash = {
+            let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() else {
+                return Err("could not run git command");
+            };
+
+            let Ok(git_hash) = String::from_utf8(output.stdout) else {
+                return Err("could not parse utf8 string from output");
+            };
+
+            String::from(git_hash.trim())
+        };
+
+        let Some(partial_git_hash) = git_hash.get(0..8) else {
+            return Err("could not get the first eight character of the git hash");
+        };
+
+        Ok(partial_git_hash.to_string())
+    }
+}
+
+fn build_revision() -> String {
+    if let Ok(revision) = env::var("BUILD_REVISION") {
+        if !revision.is_empty() {
+            return String::from(revision.trim());
+        }
+    }
+
+    if let Ok(commit_hash) = Git::rev_parse() {
+        return commit_hash;
+    }
+
+
+    return String::from("unkonwn");
+}
 
 fn main() {
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-
-    let git_hash = String::from_utf8(output.stdout)
-        .unwrap()
-        .trim()
-        .get(0..8)
-        .unwrap()
-        .to_string();
-
-    println!("cargo:rustc-env=COMMIT_HASH={}", git_hash);
+    println!("cargo:rustc-env=COMMIT_HASH={}", build_revision());
 }
